957b6fb9d15a59018e640b47e4ddb3ca61fb71fd
Ganenco Dmitri <ganenko.dmitri@gmail.com>
2020-10-07 00:32:27 +0300

    Summary

--- js/index.js
+++ js/index.js
@@ -1,57 +1,63 @@
 import bantmanList from './batman-list.js';
 
 const refs = {
-  input: document.querySelector('#filter'),
-  button: document.querySelector('.btn'),
-  list: document.querySelector('.js-container'),
+  // input: document.querySelector('#filter'),
+  // button: document.querySelector('.btn'),
+  listContainer: document.querySelector('.js-container'),
+  modalContainer: document.querySelector('js-lightbox'),
+  modalImage: document.querySelector('.lightbox__image'),
 };
+// //elem.after(nodes) - добавляет nodes после узла elem
 
-const makeProductContainerList = () => {
-  const titleList = document.createElement('h1');
-  titleList.classList.add('container-name');
-  titleList.textContent = 'Dark Knignt!';
-
-  const createList = document.createElement('ul');
-  createList.classList.add('container-list');
-
-  refs.list.append(titleList, createList); //добавил узел
-};
-
-makeProductContainerList();
-
-// const titleList = document.createElement('h1');
-// titleList.classList.add('container-name');
-// titleList.textContent = 'Dark Knignt!';
+// // const titleList = document.createElement('h1');
+// // titleList.classList.add('container__name');
+// // titleList.textContent = 'Dark Knignt!';
 
 // const createList = document.createElement('ul');
-// createList.classList.add('container-list');
+// createList.classList.add('container__list');
 
-// refs.list.append(titleList, createList);
+// refs.list.append(createList); //добавил узел
 
-const containerList = document.querySelector('ul');
+// Создание и рендер разметки по массиву данных и предоставленному шаблону.
+// const containerList = document.querySelector('ul');
 const galleryMarkup = createGalleryMarkup(bantmanList);
-containerList.insertAdjacentHTML('afterend', galleryMarkup);
+refs.listContainer.insertAdjacentHTML('beforeend', galleryMarkup);
 
 function createGalleryMarkup(batmans) {
   return batmans
     .map(({ src, name, description }) => {
       return `
-    <li class="gallery__item">
+    <li class="container__item">
   <a
-    class="gallery__link"
+    class="container__link"
     href=""
   >
     <img
-      class="gallery__image"
+      class="container__image"
       src="${src}"
-      data-source=""
+      data-source="${src}"
       alt="${name}"
     />
-    <p>${name}</p>
-    <p>${description}</p>
+    <p  class="image__name">${name}</p>
+    <p class="image__description">${description}</p>
   </a>
 </li>
     `;
     })
     .join('');
+}
+
+// Открытие модалки
+refs.listContainer.addEventListener('click', onOpenModel);
+
+function onOpenModel(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+  refs.modalContainer.classList.add('is-open');
+
+  //Добавляем изображение на модалку
+  refs.modalImage.src = e.target.dataset.sourse;
+  refs.modalImage.alt = e.target.alt;
 }
