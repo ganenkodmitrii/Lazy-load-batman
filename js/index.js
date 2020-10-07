674ec799fe6087130a0c1268537d43d9cc282a91
Ganenco Dmitri <ganenko.dmitri@gmail.com>
2020-10-07 21:06:38 +0300

    update index.js

--- js/index.js
+++ js/index.js
@@ -20,21 +20,52 @@ function createGalleryMarkup(batmans) {
     <li class="container__item">
   <a
     class="container__link"
-    href="${src}"
-  >
+    href='${src}'
+    >
     <img
-      class="container__image"
-      src="${src}"
-      data-source="${src}"
-      alt="${name}"
+      loading="lazy"
+      class='container__image lazyload'
+      data-src='${src}'
+      alt='${name}'
+      height='100'
+      width='150'
     />
-    <p  class="image__name">${name}</p>
-    <p class="image__description">${description}</p>
+    <p class="image__name">'${name}'</p>
+    <p class="image__description">
+    '${description}'
+    </p>
   </a>
 </li>
     `;
     })
     .join('');
+}
+
+if ('loading' in HTMLImageElement.prototype) {
+  console.log('Браузер поддерживает lazyload');
+  addSrcAttrToLazyImages();
+} else {
+  console.log('Браузер НЕ поддерживает lazyload');
+  addLazySizesScript();
+}
+
+function addLazySizesScript() {
+  const script = document.createElement('script');
+  script.src =
+    'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.2.2/lazysizes.min.js';
+  script.integrity =
+    'sha512-TmDwFLhg3UA4ZG0Eb4MIyT1O1Mb+Oww5kFG0uHqXsdbyZz9DcvYQhKpGgNkamAI6h2lGGZq2X8ftOJvF/XjTUg==';
+  script.crossOrigin = 'anonymous';
+
+  document.body.appendChild(script);
+}
+
+function addSrcAttrToLazyImages() {
+  const lazyImages = document.querySelectorAll('img[loading="lazy"]');
+
+  lazyImages.forEach(img => {
+    img.src = img.dataset.src;
+  });
 }
 
 refs.galleryList.addEventListener('click', onOpenModal);
@@ -49,10 +80,10 @@ function onOpenModal(evt) {
   refs.modalContainer.classList.add('is-open');
 
   // Подмена значения атрибута src элемента img.lightbox__image.
-  refs.modalImage.src = evt.target.dataset.source;
+  refs.modalImage.src = evt.target.dataset.src;
   refs.modalImage.alt = evt.target.alt;
 
-  // window.addEventListener('keydown', onCloseModalByEscape);
+  window.addEventListener('keydown', onCloseModalByEscape);
 }
 
 // Закрытие модального окна по клику на кнопку button[data-action="close-lightbox"].
@@ -61,8 +92,29 @@ refs.modalCloseBtn.addEventListener('click', onCloseModal);
 function onCloseModal() {
   refs.modalContainer.classList.remove('is-open');
 
-  // Очистка значения атрибута src элемента img.lightbox__image.
+  //   // Очистка значения атрибута src элемента img.lightbox__image.
   refs.modalImage.src = '';
   refs.modalImage.alt = '';
   window.removeEventListener('keydown', onCloseModalByEscape);
+}
+
+// Закрытие модального окна по клику на div.lightbox__overlay.
+
+refs.modalOverlay.addEventListener('click', onCloseModalByClickOverlay);
+
+function onCloseModalByClickOverlay(evt) {
+  if (evt.currentTarget === evt.target) {
+    onCloseModal();
+  }
+}
+
+// Закрытие модального окна по нажатию клавиши ESC.
+
+function onCloseModalByEscape(evt) {
+  const ESC_KEY_CODE = 'Escape';
+  const isEscKey = evt.code === ESC_KEY_CODE;
+
+  if (isEscKey) {
+    onCloseModal();
+  }
 }
